e8dbbf7237c8ea39b0506f42c9ffb5fd8fdf5dce
James Christopher Adduono <jc@adduono.com>
2016-10-19 01:37:49 -0400

    Use API 21
    Change-Id: I1c6b4b9ec1fa5c2c9343d02c9ac8cdacdec7308f

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := mkbootimg
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SRC_FILES := mkbootimg.c mincrypt/sha.c
+LOCAL_SDK_VERSION := 21
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
@@ -11,5 +12,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := unpackbootimg
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SRC_FILES := unpackbootimg.c mincrypt/sha.c
+LOCAL_SDK_VERSION := 21
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
